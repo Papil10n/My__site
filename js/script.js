ab5e3b1cd5b92b9c3b623bff8a654b0e3235c23f
Papil10n <nikept.co@gmail.com>
2022-09-21 17:05:39 +0300

    add block overflow on burger menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 const burger = document.querySelector(".icon-menu");
 const burgerList = document.querySelector(".menu__body");
+const bodyOverflow = document.body.style.overflow;
 
 burger.addEventListener("click", (event) => {
    burger.classList.toggle("_active");
    burgerList.classList.toggle("_active");
+
+   if (document.body.style.overflow == "hidden") {
+      document.body.style.overflow = '';
+   } else {
+      document.body.style.overflow = 'hidden';
+   }
+
+   console.log(document.body.style.overflow)
 })
+
